1e5dd01f1040be3ee19e1478eff8831a92fa11a9
Syed Junaid <sjunaiduk@gmail.com>
2023-01-21 17:56:26 +0000

    wrote simple test to check if a sale can be created with the correct values

---
+++ truffle/migrations/1_deploy_escrow_transactions.js
@@ -0,0 +1,13 @@
+const EscrowTransactions = artifacts.require("EscrowTransactions");
+
+module.exports = function (deployer) {
+
+
+  
+  deployer.deploy(EscrowTransactions);
+
+
+
+
+
+};
--- truffle/test/simplestorage.js
+++ truffle/test/simplestorage.js
@@ -1,18 +1,36 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
 
-contract('SimpleStorage', () => {
-  it('should read newly written values', async() => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
-    var value = (await simpleStorageInstance.read.call()).toNumber();
 
-    assert.equal(value, 6, "0 wasn't the initial value");
+const EscrowTransactions = artifacts.require("EscrowTransactions");
 
-    await simpleStorageInstance.write(1);
-    value = (await simpleStorageInstance.read.call()).toNumber();
-    assert.equal(value, 1, "1 was not written");
+contract('EscrowTransactions', () => {
+  it('should create a new escrow transactions with correct values', async() => {
+    const escrowTransactionsInstance = await EscrowTransactions.deployed();
+    const presaleAddress = '0x0000000000000000000000000000000000000123';
+    const walletToAdd = '0x0000000000000000000000000000000000009999'
+    const price = '1000000000000000000';
+    const seller = '0xc319d186f4d66863f60bdd4daccf74142c477b28';
 
-    await simpleStorageInstance.write(2);
-    value = (await simpleStorageInstance.read.call()).toNumber();
-    assert.equal(value, 2, "2 was not written");
+    await escrowTransactionsInstance.createSale(presaleAddress, walletToAdd, price);
+
+    const sale = await escrowTransactionsInstance.getSaleInfo(seller, presaleAddress,walletToAdd);
+
+    /*struct SaleInfo {
+    bool buyerAcceptedSaleAndSentBnbToContract;
+    bool cancelled;
+    bool walletAdded;
+    string presalePlatform;
+    bool moneySentToSellerByContract;
+    uint256 price;
+} */
+    assert.equal(sale.price, price, "Sale price is not correct");
+    assert.equal(sale.presalePlatform, "Pink", "Presale platform is not correct");
+    assert.equal(sale.buyerAcceptedSaleAndSentBnbToContract, false, "Buyer has not accepted sale and sent BNB to contract");
+    assert.equal(sale.cancelled, false, "Sale hasn't been cancelled");
+    assert.equal(sale.walletAdded, false, "Wallet has not been added");
+    assert.equal(sale.moneySentToSellerByContract, false, "Money has not been sent to seller by contract");
+
+
+
   });
-});
+
+  });
