0af6649302b0816a686420b4d8d3f1c09b95f730
Syed Junaid <sjunaiduk@gmail.com>
2023-01-24 01:21:26 +0000

    Changed artifact reference in react code

--- client/src/contexts/EthContext/EthProvider.jsx
+++ client/src/contexts/EthContext/EthProvider.jsx
@@ -34,7 +34,7 @@ function EthProvider({ children }) {
     
     const tryInit = async () => {
       try {
-        const artifact = require("../../contracts/SimpleStorage.json")
+        const artifact = require("../../contracts/EscrowTransactionsV2.json")
         init(artifact);
       } catch (err) {
         console.error(err);
