924767d140c2dc81664628c5573d410261b75133
Syed Junaid <sjunaiduk@gmail.com>
2023-02-01 05:31:56 +0000

    added manual check to for WL, slower but incase saul makes method view only private?

--- backend/WhitelistChecker/index.js
+++ backend/WhitelistChecker/index.js
@@ -38,9 +38,45 @@ const contract = new web3.eth.Contract(abi, contractAddress);
 // stor17 is hardcap
 // stor22 is cancelled/completed
 // store24 is how much is raised
+// stor 38 are stored addressed array
 //https://bscscan.com/bytecode-decompiler?a=0xfb9e73f0fbea4a4d9f6ae4670183662f96ecd97e
 
-web3.eth.getStorageAt(contractAddress, 24).then((r) => {
+var key = "0x09B1Fea04fBed7FfFAbFc108fD5040808300eA61";
+var slot = 39;
+
+// get length of array
+web3.eth.getStorageAt(contractAddress, 38).then((r) => {
   //convert hex to decimal
   console.log(parseInt(r, 16));
 });
+
+/// get the value at the index
+
+async function manualWhitelistCheck() {
+  const length = parseInt(await web3.eth.getStorageAt(contractAddress, 38), 16);
+  console.log(length);
+  let index = 0;
+  var compositeKey = web3.utils.soliditySha3({
+    type: "uint256",
+    value: 38,
+  });
+
+  for (let i = 0; i < length; i++) {
+    const address = await web3.eth.getStorageAt(contractAddress, compositeKey);
+    console.log(address);
+    compositeKey = new web3.utils.BN(compositeKey).add(new web3.utils.BN(1));
+  }
+}
+
+//manualWhitelistCheck();
+
+// doesnt work.........
+
+var compositeKey = web3.utils.soliditySha3({
+  type: "uint256",
+  value: 39,
+});
+
+console.log(compositeKey);
+
+web3.eth.getStorageAt(contractAddress, compositeKey).then(console.log);
