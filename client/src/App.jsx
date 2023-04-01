0e1e1cca01c3bc2066c4aae30499da4b2b7694b4
Syed Junaid <sjunaiduk@gmail.com>
2023-04-01 19:08:35 +0100

    added default route to home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
                 />
               )}
 
-              <Route path="/" element={<Home isUserSeller={seller} />} />
+              <Route path="*" element={<Home isUserSeller={seller} />} />
             </Routes>
           </>
         ) : (
