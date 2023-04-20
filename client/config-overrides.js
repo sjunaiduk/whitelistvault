98cb69889e8198ae4b5140d94c19904cc309d9eb
Syed Junaid <sjunaiduk@gmail.com>
2023-04-20 19:58:37 +0100

    removed unused library and removed unnessesary log of webpackconfig

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,6 +2,5 @@ module.exports = function override(config, env) {
   if (env === "production") {
     config.devtool = false;
   }
-  console.log(JSON.stringify(config, null, 2));
   return config;
 };
--- client/src/components/ClipBoardText.jsx
+++ client/src/components/ClipBoardText.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-
-import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Tooltip } from "antd";
 
 export const ClipBoardText = ({ text }) => {
