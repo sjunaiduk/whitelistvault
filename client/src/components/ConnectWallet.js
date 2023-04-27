9efb2011a95bd2b23edabb3e9f67afc791e294d4
Syed Junaid <sjunaiduk@gmail.com>
2023-04-27 19:14:01 +0100

    added google tag manager data layer push when user connects wallet

--- client/src/components/ConnectWallet.js
+++ client/src/components/ConnectWallet.js
@@ -1,13 +1,25 @@
 import { useWeb3Modal } from "@web3modal/react";
+import { useEffect } from "react";
 
-import { useAccount, useDisconnect } from "wagmi";
+import { useAccount, useDisconnect, useConnect } from "wagmi";
 import { bsc } from "wagmi/chains";
 
 export const ConnectWallet = () => {
   const { disconnect } = useDisconnect();
   const { address } = useAccount();
+  const { isSuccess } = useConnect();
   const { open, setDefaultChain } = useWeb3Modal();
   setDefaultChain(bsc);
+
+  useEffect(() => {
+    if (isSuccess) {
+      window.dataLayer.push({
+        event: "userWallet",
+        user_id: address,
+      });
+    }
+  }, [isSuccess]);
+
   return (
     <>
       {address ? (
